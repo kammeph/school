15cf8085aa738ab0b392a833755ceae250d70929
kammeph <ph.kammerer@googlemail.com>
2023-03-03 15:07:12 +0100

    feat: added schools shell module

---
+++ libs/school-book-storage/schools/feature/shell/src/lib/schools-shell.module.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { SchoolsShellModule } from './schools-shell.module';
+
+describe('SchoolBookStorageSchoolsFeatureShellModule', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SchoolsShellModule],
+    }).compileComponents();
+  });
+
+  // TODO: Add real tests here.
+  //
+  // NB: This particular test does not do anything useful.
+  //     It does NOT check for correct instantiation of the module.
+  it('should have a module definition', () => {
+    expect(SchoolsShellModule).toBeDefined();
+  });
+});
