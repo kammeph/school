6a0d65ff8ee7e63a0a642fcfb42fa2302ef4a1fc
kammeph <ph.kammerer@googlemail.com>
2023-03-06 20:38:42 +0100

    feat: added school list component

--- libs/school-book-storage/schools/feature/list/src/lib/school-list.module.ts
+++ libs/school-book-storage/schools/feature/list/src/lib/school-list.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SchoolListRoutingModule } from './school-list-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { SchoolListComponent } from './school-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    IonicModule,
+    ReactiveFormsModule,
+    SchoolListRoutingModule,
+  ],
+  declarations: [SchoolListComponent],
 })
 export class SchoolListModule {}
